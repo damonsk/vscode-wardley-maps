6e67c6df89e363ad848ff97aff89c4e57ff00de4
Preston Doster <pdoster@twilio.com>
2024-01-05 13:26:48 -0600

    applied prettier suggestions

--- view/viewLoader.js
+++ view/viewLoader.js
@@ -2,87 +2,93 @@ const vscode = require('vscode');
 const path = require('path');
 
 class ViewLoader {
-    constructor(context, editor) {
-        this._extensionPath = context.extensionPath;
-        this._editor = editor;
-        this._panel = vscode.window.createWebviewPanel(
-          "mapView",
-          "Map View",
-          vscode.ViewColumn.Two,
-          {
-            enableScripts: true,
-            localResourceRoots: [
-              vscode.Uri.file(path.join(this._extensionPath, 'build'))
-            ]
-          }
-        );
-    
-        this._panel.webview.html = this.getWebviewContent();
-        // Handle messages from the webview
-        this._panel.webview.onDidReceiveMessage(
-          message => {
-            switch (message.command) {
-              case 'updateText':
-                const textEditor = this._editor;
-                var firstLine = textEditor.document.lineAt(0);
-                var lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
-                var textRange = new vscode.Range(0,
-                    firstLine.range.start.character,
-                    textEditor.document.lineCount - 1,
-                    lastLine.range.end.character);
+	constructor(context, editor) {
+		this._extensionPath = context.extensionPath;
+		this._editor = editor;
+		this._panel = vscode.window.createWebviewPanel(
+			'mapView',
+			'Map View',
+			vscode.ViewColumn.Two,
+			{
+				enableScripts: true,
+				localResourceRoots: [
+					vscode.Uri.file(path.join(this._extensionPath, 'build')),
+				],
+			}
+		);
 
-                textEditor.edit( editBuilder => {
-                    editBuilder.replace(textRange, message.val);
-                });
-                return;
-            }
-          },
-          undefined, 
-          context.subscriptions
-        );
-      }
+		this._panel.webview.html = this.getWebviewContent();
+		// Handle messages from the webview
+		this._panel.webview.onDidReceiveMessage(
+			(message) => {
+				switch (message.command) {
+					case 'updateText':
+						const textEditor = this._editor;
+						var firstLine = textEditor.document.lineAt(0);
+						var lastLine = textEditor.document.lineAt(
+							textEditor.document.lineCount - 1
+						);
+						var textRange = new vscode.Range(
+							0,
+							firstLine.range.start.character,
+							textEditor.document.lineCount - 1,
+							lastLine.range.end.character
+						);
 
-      postMessage = function(message){
-        this._panel.webview.postMessage({ command: 'text', val: message });
-      }
+						textEditor.edit((editBuilder) => {
+							editBuilder.replace(textRange, message.val);
+						});
+						return;
+				}
+			},
+			undefined,
+			context.subscriptions
+		);
+	}
 
-      setActiveEditor = function(editor){
-        this._editor = editor;
-      }
+	postMessage = function (message) {
+		this._panel.webview.postMessage({ command: 'text', val: message });
+	};
 
-      getNonce = function() {
-        let text = "";
-        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (let i = 0; i < 32; i++) {
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-      }
+	setActiveEditor = function (editor) {
+		this._editor = editor;
+	};
 
-      buildUri = function(p){
-        const runtimePath = path.join(this._extensionPath, 'build', p);
-        const runtimePathOnDisk = vscode.Uri.file(runtimePath);
-        return runtimePathOnDisk.with({ scheme: 'vscode-resource' });
-      }
+	getNonce = function () {
+		let text = '';
+		const possible =
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		for (let i = 0; i < 32; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	};
 
-      getWebviewContent = function() {
-        const p = path.join(this._extensionPath, 'build', 'asset-manifest.json');
-        const manifest = require(p);
-        const scripts = manifest.entrypoints;
-        const styles = manifest.files['main.css'];
+	buildUri = function (p) {
+		const runtimePath = path.join(this._extensionPath, 'build', p);
+		const runtimePathOnDisk = vscode.Uri.file(runtimePath);
+		return runtimePathOnDisk.with({ scheme: 'vscode-resource' });
+	};
 
+	getWebviewContent = function () {
+		const p = path.join(this._extensionPath, 'build', 'asset-manifest.json');
+		const manifest = require(p);
+		const scripts = manifest.entrypoints;
+		const styles = manifest.files['main.css'];
 
-        let scriptsToInclude = scripts.map(p => this.buildUri(p));
-        let stylesToInclude = this.buildUri(styles);
-        const nonce = this.getNonce();
-        let scriptText = "";
-        scriptText = scriptText += `<link rel="stylesheet" nonce="${nonce}" href="${stylesToInclude}">`;
-        for (let index = 0; index < scriptsToInclude.length; index++) {
-            let element = scriptsToInclude[index];
-            scriptText = scriptText += `<script nonce="${nonce}" src="${element}"></script>`;
-        }
+		let scriptsToInclude = scripts.map((p) => this.buildUri(p));
+		let stylesToInclude = this.buildUri(styles);
+		const nonce = this.getNonce();
+		let scriptText = '';
+		scriptText =
+			scriptText += `<link rel="stylesheet" nonce="${nonce}" href="${stylesToInclude}">`;
+		for (let index = 0; index < scriptsToInclude.length; index++) {
+			let element = scriptsToInclude[index];
+			scriptText =
+				scriptText += `<script nonce="${nonce}" src="${element}"></script>`;
+		}
 
-        return `<!DOCTYPE html>
+		return `<!DOCTYPE html>
         <html lang="en">
         <head>
           <meta charset="utf-8">
@@ -91,7 +97,9 @@ class ViewLoader {
           <title>React App</title>
           
           <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
-          <base href="${vscode.Uri.file(path.join(this._extensionPath, 'build')).with({ scheme: 'vscode-resource' })}/">
+          <base href="${vscode.Uri.file(
+						path.join(this._extensionPath, 'build')
+					).with({ scheme: 'vscode-resource' })}/">
         </head>
         <body>
           <div id="root"></div>
@@ -114,7 +122,7 @@ class ViewLoader {
         </script>
         </body>
         </html>`;
-	}
+	};
 }
 
 module.exports = ViewLoader;
